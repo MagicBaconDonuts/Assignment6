924dcb666e39c3c97988a6619f8a3cbf09faf5c8
MagicBaconDonuts <ricardoalatorre89@gmail.com>
2021-12-17 22:50:24 -0800

    V2
    Organizes Code aka I made it cleaner

--- src/CarServices.java
+++ src/CarServices.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class CarServices {
 	Car createCar(String[] info) {
@@ -6,5 +8,31 @@ public class CarServices {
 		car.setSale(Integer.parseInt(info[1]));
 		return car;
 		
+	}
+	public int findSumForYear(List<Car> list, String year) {
+		return list.stream()
+			.filter(y -> y.getDate().contains(year))
+			.map(sales -> sales.getSale())
+			.mapToInt(Integer::valueOf)
+			.sum();
+		
+	}
+	public int findMaxValue(List<Car> carList) {
+		return carList.stream()
+		 .map(sales -> sales.getSale())
+		 .max(Integer::compare)
+		 .get();
+	}
+	public int findMinValue(List<Car> carList) {
+		return carList.stream()
+					  .map(sales -> sales.getSale())
+					  .min(Integer::compare)
+					  .get();
+	}
+	public String findCarDate(List<Car> model3Cars, int saleValue) {
+		return model3Cars.stream()
+						 .filter(price -> price.getSale().equals(saleValue))
+						 .map(date -> date.getDate())
+						 .collect(Collectors.joining());
 	}
 }
